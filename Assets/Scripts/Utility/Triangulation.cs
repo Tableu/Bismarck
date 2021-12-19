9a3b6c28ebefcbe3199bd7bee0f30b43c75a903b
Ben Kellman <kellman.ben@gmail.com>
2021-12-18 22:57:03 -0800

    Adding Delaunay Triangulation helper function - Based on DelaunatorSharp

---
+++ Assets/Scripts/Utility/Triangulation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using DelaunatorSharp;
+
+namespace Utility
+{
+    public static class Triangulation
+    {
+        public static List<HashSet<int>> Delaunay(List<Vector2> points)
+        {
+            var mapping = points.Select(_ => new HashSet<int>()).ToList();
+            var newPoints = points.Select(v => new Point(v.x, v.y)).Cast<IPoint>().ToArray();
+            var triangulation = new Delaunator(newPoints);
+
+            foreach (var halfEdge in triangulation.Halfedges)
+            {
+                var end = Delaunator.NextHalfedge(halfEdge);
+                mapping[halfEdge].Add(end);
+            }
+            
+            return mapping;
+        }
+    }
+}
