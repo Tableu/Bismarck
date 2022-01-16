6464d0fc9d3537ecfdfdfe796cc9fcf154ec25a4
Ben Kellman <kellman.ben@gmail.com>
2022-01-15 22:44:38 -0800

    Add basic loading logic to map

--- Assets/Scripts/StarMap/MapData.cs
+++ Assets/Scripts/StarMap/MapData.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 namespace StarMap
 {
-    [CreateAssetMenu(fileName = "Map", menuName = "Map/StarMap", order = 0)]
-    public class Map : ScriptableObject
+    [CreateAssetMenu(fileName = "MapData", menuName = "Map/StarMap", order = 0)]
+    public class MapData : ScriptableObject
     {
         public List<StarSystem> StarSystems;
         public List<SystemPair> SystemPairs;
+
 
         [Serializable]
         public struct SystemPair
