6a699d27da2b9948a567668638451b130dd723ae
darthvader99999 <bhow245@gmail.com>
2022-01-22 17:48:52 -0800

    Moving some random fleet logic to LoadSystem

--- Assets/Scripts/Scene/LoadEnemies.cs
+++ Assets/Scripts/Scene/LoadEnemies.cs
@@ -11,16 +11,13 @@ public class LoadEnemies : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FleetDBScriptableObject randomFleetDB = mapContext.CurrentSystem.RandomFleetDB;
         enemyShipSpawner.ProjectileParent = projectileParent;
-        RandomFleet fleet = randomFleetDB.fleetDB[Random.Range(0, randomFleetDB.fleetDB.Count)];
-        IEnumerator fleetPositions = fleet.fleetVisualsPrefab.GetComponentsInChildren<Transform>().GetEnumerator();
-        
-        foreach (RandomShipList randomShip in fleet.randomFleet)
+        IEnumerator fleetPositions = mapContext.CurrentSystem.FleetPositions.GetEnumerator();
+
+        foreach (ShipData shipData in mapContext.CurrentSystem.Fleet)
         {
             if (fleetPositions.MoveNext())
             {
-                ShipData shipData = randomShip.RandomShip();
                 GameObject ship = enemyShipSpawner.SpawnShip(shipData, fleetParent);
                 Transform spawnPos = fleetPositions.Current as Transform;
                 if (spawnPos != null)
--- Assets/Scripts/StarMap/StarSystem.cs
+++ Assets/Scripts/StarMap/StarSystem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace StarMap
 {
@@ -13,13 +15,36 @@ namespace StarMap
         public StarType MainStar;
         public float StarSize;
         public FleetDBScriptableObject RandomFleetDB;
+        public List<ShipData> Fleet;
+        public List<Transform> FleetPositions;
+
+        public void OnEnable()
+        {
+            Fleet = new List<ShipData>();
+            FleetPositions = new List<Transform>();
+        }
+
+        public void OnDisable()
+        {
+            Fleet = null;
+            FleetPositions = null;
+        }
 
         /// <summary>
         /// Instances all objects needed to load the star system
         /// </summary>
         public void LoadSystem()
         {
-            // todo: call system loading code here (ie. fleet generation ect.)
+            RandomFleet fleet = RandomFleetDB.fleetDB[Random.Range(0, RandomFleetDB.fleetDB.Count)];
+            foreach (Transform child in fleet.fleetVisualsPrefab.transform)
+            {
+                FleetPositions.Add(child);
+            }
+            
+            foreach (RandomShipList randomShip in fleet.randomFleet)
+            {
+                Fleet.Add(randomShip.RandomShip());
+            }
         }
     }
 }
