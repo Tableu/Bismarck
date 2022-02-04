8d64f0f71b97fb9f5fda1e2b1d666376353304cc
Ben Kellman <kellman.ben@gmail.com>
2022-02-03 19:37:13 -0800

    Refactor IdList & UniqueId scriptable objects - Replaced internal list with dictionary on IdList - Made it so the id are assigned to lists from the ID object instead of the list

--- Assets/Scripts/Systems/IdList.cs
+++ Assets/Scripts/Systems/IdList.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Systems
@@ -8,47 +8,26 @@ namespace Systems
     ///     A scriptable object used store lists of ID objects. Automatically sets the parentList property of the UUID object
     ///     to the list when it is added to the list via the inspector.
     /// </summary>
+    /// <remarks>
+    ///     This system is intended to be used to save references to scriptable objects when generating save files.
+    ///     It is a slightly nicer solution than serializing references to Scriptable Objects since it gives us complete control
+    ///     over the data & results in more readable save files.
+    /// </remarks>
     [CreateAssetMenu(fileName = "New ID List", menuName = "ID List", order = 0)]
     public class IdList : ScriptableObject
     {
-        public List<UniqueId> uniqueObjects;
+        [NonSerialized] private Dictionary<string, UniqueId> _idMap = new Dictionary<string, UniqueId>();
 
-        /// <summary>
-        ///     Called when values are changed in the inspector, also called by UniqueId on validate function.
-        ///     Verifies that all IDs are unique and prints an error if they are not.
-        /// </summary>
-        [ContextMenu("Validate IDs")]
-        public void OnValidate()
+        public IReadOnlyDictionary<string, UniqueId> IDMap => _idMap;
+        public void Add(UniqueId id)
         {
-            var duplicateIds = new List<string>();
-            foreach (var uniqueObject in uniqueObjects)
-            {
-                if (uniqueObject.parentList != null && uniqueObject.parentList != this)
-                {
-                    Debug.LogWarning($"Attempting to add {uniqueObject.id} to multiple lists ({name} and {uniqueObject.parentList.name})");
-                }
-                var idCount = uniqueObjects.Count(o => o.id == uniqueObject.id);
-                if (!duplicateIds.Contains(uniqueObject.id) && idCount != 1)
-                {
-                    Debug.LogError($"Found {idCount} occurrences of \"{uniqueObject.id}\" in {name}");
-                    duplicateIds.Add(uniqueObject.id);
-                }
-                uniqueObject.parentList = this;
-            }
+            Debug.Assert(!_idMap.ContainsKey(id.id), $"IdList already contains key {id.id}");
+            _idMap[id.id] = id;
         }
 
-        /// <summary>
-        ///     Gets a scriptable object from is id.
-        /// </summary>
-        /// <param name="id">The scriptable object uuid</param>
-        /// <returns>A scriptable object with the given id or null if no such object could be found</returns>
-        public UniqueId FindById(string id)
+        public void Remove(UniqueId id)
         {
-            if (id is null)
-            {
-                return null;
-            }
-            return uniqueObjects.FirstOrDefault(scriptableObject => scriptableObject.id == id);
+            _idMap.Remove(id.id);
         }
     }
 }
