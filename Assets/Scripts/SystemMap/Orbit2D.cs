4c077aff1bbb0800d2c889a34f08d428466e9d21
Ben Kellman <kellman.ben@gmail.com>
2022-02-16 22:40:50 -0800

    Add basic Orbit2D utility class

---
+++ Assets/Scripts/SystemMap/Orbit2D.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using UnityEngine;
+
+namespace SystemMap
+{
+    public class Orbit2D
+    {
+        private CartesianElements _cartesianElements;
+        private KeplerianElements _keplerianElements;
+
+        private double _mu;
+        private ValidElements _valid;
+
+        public Orbit2D(KeplerianElements keplerianElements, double mu)
+        {
+            _keplerianElements = keplerianElements;
+            _mu = mu;
+            _valid = ValidElements.Keplerian;
+        }
+
+        public Orbit2D(CartesianElements cartesianElements, double mu)
+        {
+            _cartesianElements = cartesianElements;
+            _mu = mu;
+            _valid = ValidElements.Cartesian;
+        }
+
+        public CartesianElements Cartesian
+        {
+            get
+            {
+                if ((_valid & ValidElements.Cartesian) == ValidElements.None)
+                {
+                    UpdateCartesian();
+                }
+                return _cartesianElements;
+            }
+        }
+
+        public KeplerianElements Keplerian
+        {
+            get
+            {
+                if ((_valid & ValidElements.Keplerian) == ValidElements.None)
+                {
+                    UpdateKeplerian();
+                }
+                return _keplerianElements;
+            }
+        }
+
+        public void UpdateFromElements(KeplerianElements keplerianElements)
+        {
+            _keplerianElements = keplerianElements;
+            _valid = ValidElements.Keplerian;
+        }
+
+        public void UpdateFromElements(CartesianElements cartesianElements)
+        {
+            _cartesianElements = cartesianElements;
+            _valid = ValidElements.Cartesian;
+        }
+
+        public void GetPointsOnOrbit(Vector3[] points, int numPoints)
+        {
+            if ((_valid & ValidElements.Keplerian) == ValidElements.None)
+            {
+                UpdateCartesian();
+            }
+            var inc = 2f * Mathf.PI / numPoints;
+            float eccentricAnomaly = 0;
+
+            for (int i = 0; i < numPoints; i++)
+            {
+                eccentricAnomaly += inc;
+                var v = ComputeTrueAnomaly(eccentricAnomaly, _keplerianElements.eccentricity);
+                var r = _keplerianElements.semimajorAxis * (1 - _keplerianElements.eccentricity * Math.Cos(eccentricAnomaly));
+
+                points[i] = new Vector3(
+                    (float)(r * Math.Cos(v + _keplerianElements.argumentOfPeriapsis)),
+                    (float)(r * Math.Sin(v + _keplerianElements.argumentOfPeriapsis)),
+                    0);
+            }
+        }
+        private void UpdateCartesian()
+        {
+
+            var e = _keplerianElements.eccentricity;
+            var a = _keplerianElements.semimajorAxis;
+            var w = _keplerianElements.argumentOfPeriapsis;
+
+            var E = KeplerSolver(_keplerianElements.meanAnomaly, e);
+            var trueAnomaly = ComputeTrueAnomaly(E, e);
+
+            var rc = a * (1 - e * Math.Cos(E));
+
+            var vx = Math.Sqrt(_mu * a) / rc * -1 * Math.Sin(E);
+            var vy = Math.Sqrt(_mu * a) / rc * Math.Sqrt(1 - e * e) * Math.Cos(E);
+
+
+            _cartesianElements = new CartesianElements
+            {
+                x = rc * Math.Cos(trueAnomaly + w),
+                y = rc * Math.Sin(trueAnomaly + w),
+                vx = vx * Math.Cos(w) - vy * Math.Sin(w),
+                vy = vx * Math.Sin(w) + vy * Math.Cos(w),
+            };
+            _valid |= ValidElements.Cartesian;
+        }
+
+        private void UpdateKeplerian()
+        {
+            double h = _cartesianElements.x * _cartesianElements.vy - _cartesianElements.y * _cartesianElements.vx;
+
+            double rmag = Math.Sqrt(_cartesianElements.x * _cartesianElements.x + _cartesianElements.y * _cartesianElements.y);
+            double ex = (h * _cartesianElements.vy) / _mu - _cartesianElements.x / rmag;
+            double ey = (-h * _cartesianElements.vx) / _mu - _cartesianElements.y / rmag;
+            var emag = Math.Sqrt(ex * ex + ey * ey);
+
+            double v;
+            if (emag != 0)
+            {
+                v = Math.Acos((ex * _cartesianElements.x + ey * _cartesianElements.y) / (emag * rmag));
+            }
+            else
+            {
+                v = Math.Acos(_cartesianElements.x / rmag);
+            }
+            var w = Math.Atan2(ey, ex);
+            if (_cartesianElements.x * _cartesianElements.vx + _cartesianElements.y * _cartesianElements.vy < 0)
+            {
+                v = 2 * Math.PI - v;
+                w = 2 * Math.PI - w;
+            }
+
+            var E = 2 * Math.Atan(Math.Tan(v / 2) / Math.Sqrt((1 + emag) / (1 - emag)));
+            var M = E - emag * Math.Sin(E);
+            var vmag_sqr = _cartesianElements.vy * _cartesianElements.vy + _cartesianElements.vx * _cartesianElements.vx;
+            var a = 1 / (2 / rmag - vmag_sqr / _mu);
+
+            _keplerianElements = new KeplerianElements
+            {
+                eccentricity = emag,
+                meanAnomaly = M,
+                semimajorAxis = a,
+                argumentOfPeriapsis = w
+            };
+            _valid |= ValidElements.Keplerian;
+        }
+        private static double KeplerSolver(double M, double e, double accuracy = 1e-6, int maxIter = 100)
+        {
+            double E = M;
+            double Enext;
+
+            double delta = 1;
+            int count = 0;
+            while (delta > accuracy && count < maxIter)
+            {
+                Enext = E - (E - e * Math.Sin(E) - M) / (1 - e * Math.Cos(E));
+                delta = Math.Abs(E - Enext);
+                E = Enext;
+                count++;
+            }
+            Debug.Log($"Took {count} iterations");
+
+            return E;
+        }
+        private static double ComputeTrueAnomaly(double eccentricAnomaly, double eccentricity)
+        {
+            return 2 * Math.Atan2(
+                Math.Sqrt(1 + eccentricity) * Math.Sin(eccentricAnomaly / 2),
+                Math.Sqrt(1 - eccentricity) * Math.Cos(eccentricAnomaly / 2)
+            );
+        }
+
+        [Serializable]
+        public struct KeplerianElements
+        {
+            public double eccentricity;
+            public double semimajorAxis;
+            public double argumentOfPeriapsis;
+            public double meanAnomaly;
+        }
+
+        [Serializable]
+        public struct CartesianElements
+        {
+            public double x;
+            public double y;
+            public double vx;
+            public double vy;
+        }
+
+        [Flags]
+        private enum ValidElements
+        {
+            None = 0,
+            Keplerian = 1 << 0,
+            Cartesian = 1 << 1
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/TestOrbit2D.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+using SystemMap;
+
+namespace Tests.EditMode
+{
+    public class TestOrbit2D
+    {
+
+        [Test]
+        public void CartesianFromKeplerianElements()
+        {
+            var orbit = new Orbit2D(new Orbit2D.KeplerianElements
+            {
+                eccentricity = 0,
+                meanAnomaly = 0,
+                semimajorAxis = 1,
+                argumentOfPeriapsis = 0
+            }, 1);
+
+            var expected = new Orbit2D.CartesianElements
+            {
+                x = 1,
+                y = 0,
+                vx = 0,
+                vy = 1
+            };
+            var actual = orbit.Cartesian;
+            Assert.AreEqual(expected.x, actual.x);
+            Assert.AreEqual(expected.y, actual.y);
+            Assert.AreEqual(expected.vx, actual.vx);
+            Assert.AreEqual(expected.vy, actual.vy);
+        }
+
+        [Test]
+        public void KeplerianFromCartesianElements()
+        {
+            var orbit = new Orbit2D(new Orbit2D.CartesianElements
+            {
+                x = 1,
+                y = 0,
+                vx = 0,
+                vy = 1
+            }, 1);
+
+            var expected = new Orbit2D.KeplerianElements
+            {
+                eccentricity = 0,
+                meanAnomaly = 0,
+                semimajorAxis = 1,
+                argumentOfPeriapsis = 0
+            };
+
+            var actual = orbit.Keplerian;
+            Assert.AreEqual(expected.eccentricity, actual.eccentricity);
+            Assert.AreEqual(expected.meanAnomaly, actual.meanAnomaly);
+            Assert.AreEqual(expected.semimajorAxis, actual.semimajorAxis);
+            Assert.AreEqual(expected.argumentOfPeriapsis, actual.argumentOfPeriapsis);
+        }
+
+        [TestCase(0, 1, 0, 0)]
+        [TestCase(0.85, 25, 0, 0)]
+        [TestCase(0.85, 25, Math.PI / 4, 0)]
+        [TestCase(0.85, 25, Math.PI / 4, Math.PI / 4)]
+        public void ConvertBetweenKeplerianAndCartesian(double e, double a, double w, double M)
+        {
+            var orbit = new Orbit2D(new Orbit2D.KeplerianElements
+            {
+                eccentricity = e,
+                meanAnomaly = M,
+                semimajorAxis = a,
+                argumentOfPeriapsis = w
+            }, 1);
+
+            var cartesian = orbit.Cartesian;
+            orbit.UpdateFromElements(cartesian);
+            var actual = orbit.Keplerian;
+            Assert.That(actual.eccentricity, Is.EqualTo(e).Within(1e-4));
+            Assert.That(actual.meanAnomaly, Is.EqualTo(M).Within(1e-4));
+            Assert.That(actual.semimajorAxis, Is.EqualTo(a).Within(1e-4));
+            Assert.That(actual.argumentOfPeriapsis, Is.EqualTo(w).Within(1e-4));
+        }
+    }
+}
